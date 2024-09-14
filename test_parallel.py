aa91f9278ba3ba0381635524a8005f1fc2b3d2d8
Zhitu Ma <zhma@wakehealth.edu>
2024-09-14 08:19:51 -0400

    clean-up local folder

--- test_parallel.py
+++ test_parallel.py
@@ -6,7 +6,7 @@ import onnxruntime
 from transformers.pipelines.token_classification import AggregationStrategy
 from multiprocessing.pool import Pool
 from functools import partial
-from datetime import datetime
+from datetime import datetime   
 
 def load_onnx_model(onnx_model_path, onnx_model_name, num_threads=4, device_id=0):
     options = onnxruntime.SessionOptions()
@@ -37,21 +37,45 @@ if (__name__ == '__main__'):
     tokenizer.model_max_length = 128
     model = load_onnx_model(distill_onnx_path, 'model_optimized.onnx')
     clf = MyTokenClassificationPipeline_GPU(model=model, tokenizer=tokenizer, device=device_id)
-
-    print(datetime.now())
-    results = clf(all_texts, stride=16, ignore_labels=['NORMAL'], aggregation_strategy='max', batch_size=16)
-    print(datetime.now())
-
     pool = Pool(processes=2)
     tmp = MyTokenClassificationPipeline_CPU(model=model, tokenizer=tokenizer)
     func = partial(tmp.postprocess, aggregation_strategy=AggregationStrategy.MAX, ignore_labels=['NORMAL'])
 
-    print(datetime.now())
-    rr = pool.imap(func, results, chunksize=50)
-    rr = list(rr)
-    print(datetime.now())
+    rr = None
+    rr_all = []
+
+    print(f'before running: {datetime.now()}')
+
+    for k in range(3):
+        print(f'===== new round {k} =====')
+        print('before gpu:', datetime.now())
+        results = clf(all_texts, stride=16, ignore_labels=['NORMAL'], aggregation_strategy='max', batch_size=16)
+        print('after gpu:', datetime.now())
+
+        if rr is not None:
+            print('before wait', datetime.now())
+            rr.wait()
+            print('after wait', datetime.now())
+            rr_all.extend(list(rr.get()))
+
+        print('before map async:', datetime.now())
+        rr = pool.map_async(func, results, chunksize=50)
+        # rr.wait()
+        # rr_all.extend(list(rr.get()))
+        print('after map async:', datetime.now())
+
+    # last chunk of cpu process
+    print('before last trunk:', datetime.now())
+    rr.wait()
+    rr_all.extend(list(rr.get()))
+    print('after last trunk:', datetime.now())
+
+    print(f'after running: {datetime.now()}')
 
     # print(rr[:10])
     pool.close()
+    print(rr_all[7])
+    print(rr_all[2007])
+    print(rr_all[4007])
 
-    print(rr[111])
+
